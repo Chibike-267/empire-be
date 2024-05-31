f819e8ed06e9ae57c07ae8add7979011a53d14d2
Chibike-267 <paulinuschukwubuikem98@gmail.com>
2024-05-31 16:10:05 +0100

    project

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,25 @@ import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
 import * as path from 'path';
+import * as cors from 'cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(4000);
+
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
