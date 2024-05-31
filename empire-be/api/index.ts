94e324aacfbadad74d606d41fa2a1f6a5d71a48a
Chibike-267 <paulinuschukwubuikem98@gmail.com>
2024-05-31 19:23:50 +0100

    project

---
+++ empire-be/api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const server = express();
+
+const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  await app.init();
+};
+
+createNestServer(server);
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  server(req, res);
+};
